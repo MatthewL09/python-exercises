3504701ca3bc318c4ae86c501a4f37e9de3760fd
MatthewL09 <matthew.r.luna09@gmail.com>
2022-01-18 16:53:14 -0600

    update

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -10,3 +10,20 @@ facebook = 350
 
 (google * 6) + (amazon * 4) + (facebook * 10)
 
+
+
+# continued work
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+len(username) > 5
+
+len(password) <= 20
+
+password != username
+
+#bonus
+password[-1][0] != " "
+username [0][-1] != " "
+
