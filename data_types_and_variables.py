0381fe8d89a3c8efb2107215b40a5aa815523b3c
MatthewL09 <matthew.r.luna09@gmail.com>
2022-01-20 09:10:46 -0600

    update

--- data_types_and_variables.py
+++ data_types_and_variables.py
@@ -27,3 +27,91 @@ password != username
 password[-1][0] != " "
 username [0][-1] != " "
 
+
+type(99.9)
+
+type("False")
+
+type('0')
+
+type('True')
+
+type([{}])
+
+type({'a':[]})
+
+# string
+
+# bool
+
+# float
+
+# bool
+
+# string
+
+# float
+
+# list
+
+# string
+
+# dictionary
+
+price_per_day = 3.0
+mermaid_days = 3
+bear_days = 5
+herc_days = 1
+
+total days = mermaid_days + bear_days +herc_days
+amount_spent = total_days * price_per_day
+amount_spent
+
+little_mermaid = 3 * 3
+brother_bear = 5 * 3
+hercules = 1 * 3
+
+hercules + brother_bear + little_mermaid
+
+google_rate = 400
+amazon_rate = 380
+facebook_rate = 350
+
+google_hours = 6
+amazon_hours = 4
+facebook_hours = 10
+
+total_pay = (google_rate * google_hours) + (amazon_rate * amazon_hours) + (facebook_rate * facebook_hours)
+total_pay
+
+
+class_has_room = True
+schedule_has_room = False
+
+can_enroll = class_has_room and schedule_has_room
+can_enroll
+
+more_than_two = True
+premium_member = False
+offer_not_expired = True
+discount_valid = offer_not_expired and (premium_member or more_than_two)
+discount_valid
+
+
+
+# continued work
+
+username = 'codeup'
+password = 'notastrongpassword'
+
+username_is_long_enough = len(username) > 5
+
+password_is_long_enough = len(password) <= 20
+
+username_and_password_are_diff = password != username
+
+#bonus
+password_has_spaces = password != password.strip()
+username_has_spaces = username != username.strip()
+
+username_good = username_and_password_are_diff and username_is_short_enough and not username_has_spaces
